f99f87c99f805604abfcdc0be291cd3992a2cbea
Kayla Cross <kac288@georgetown.edu>
2018-02-04 23:20:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,22 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #left column
+  [1,4,7], #middle column
+  [2,5,8], #right column
+  [0,4,8], #right diagonal
+  [2,4,6] #left diagonal
+]
+
+def won?(board)
+  for win_combination in WIN_COMBINATIONS
+    for index in win_combination
+      board.each_with_index do |space, index|
+        puts "Contents: #{space}, Index; #{index}""
+    end
+  end
+end
